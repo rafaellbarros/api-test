6a68ebf31cfc34f343be89de780d12d26725d877
Rafael Barros <rafaelbarros.df@gmail.com>
2021-06-23 10:13:55 -0300

    :construction: Implementando APITest

---
+++ src/test/java/br/com/rafaellbarros/tasks/apitest/APITest.java
@@ -0,0 +1,38 @@
+package br.com.rafaellbarros.tasks.apitest;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class APITest {
+
+    @BeforeClass
+    public static void setUp() {
+        RestAssured.baseURI = "http://localhost:8001/tasks-backend";
+    }
+
+    @Test
+    public void deveRetornarTarefas() {
+        RestAssured.given()
+                .when()
+                    .get("/todo")
+                .then()
+                    .statusCode(200)
+                ;
+    }
+
+    @Test
+    public void deveAdicionarTarefaComSucesso() {
+        RestAssured.given()
+                .body("{ \"task\": \"Teste via API\", \"dueDate\": \"2021-06-23\"}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/todo")
+                .then()
+                .statusCode(201)
+        ;
+    }
+}
+
+
