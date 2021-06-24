27c587107c514f1007697d3791f9273c5aa1ef8c
Rafael Barros <rafaelbarros.df@gmail.com>
2021-06-24 09:54:45 -0300

    :construction: Add teste remoção

--- src/test/java/br/com/rafaellbarros/tasks/apitest/APITest.java
+++ src/test/java/br/com/rafaellbarros/tasks/apitest/APITest.java
@@ -35,14 +35,12 @@ public class APITest {
                 .then()
                     .statusCode(201)
         ;
-
-
     }
 
     @Test
     public void naoDeveAdicionarTarefaInvalida() {
         RestAssured.given()
-                .body("{ \"task\": \"Teste via API\", \"dueDate\": \"2010-06-23\"}")
+                .body("{ \"task\": \"Teste via API\", \"dueDate\": \""+LocalDate.now().minusDays(1)+"\"}")
                     .contentType(ContentType.JSON)
                 .when()
                     .post("/todo")
@@ -50,6 +48,30 @@ public class APITest {
                     .statusCode(400)
                     .body("message", CoreMatchers.is("Due date must not be in past"))
         ;
+    }
+
+    @Test
+    public void deveRemoverTarefaComSucesso() {
+
+        final Integer id = RestAssured.given()
+                .body("{ \"task\": \"Tarefa para remoção\", \"dueDate\": \""+LocalDate.now()+"\"}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/todo")
+                .then()
+                // .log().all()
+                .statusCode(201)
+                .extract().path("id")
+        ;
+
+        System.out.println(id);
+
+        // remover
+        RestAssured.given()
+                .when()
+                .delete("/todo/"+id)
+                .then()
+                .statusCode(204);
 
 
     }
